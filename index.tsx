e1f6082e243dfb2debd81f4ec810e21c9a73bb71
Sahil Prasad <sahil@continualiq.com>
2019-11-21 17:44:44 -0800

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    const [loading, setLoading] = React.useState(false);
+
+    return (
+        <div>Help</div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+)
+
